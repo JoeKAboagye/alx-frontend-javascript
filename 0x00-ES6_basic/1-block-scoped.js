aee29fbebece0206aba83cc978a8162220bc1bf5
Joseph Aboagye <josephkwabenaaboagye@gmail.com>
2022-04-04 23:01:26 +0000

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /* eslint-disable-variable-change */
     const task = true;
     const task2 = false;
+    /* eslint-disable-variable-change */
   }
 
   return [task, task2];
