ef6643695a07adc44fe5e7217fcc6954dc26af01
Joseph Aboagye <josephkwabenaaboagye@gmail.com>
2022-04-12 17:46:28 +0000

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,5 @@
 const getStudentIdsSum = (students) => students.reduce(
-    (previousValue, currentValue) => previousValue + currentValue.id, 0
+  (previousValue, currentValue) => previousValue + currentValue.id, 0
 );
 
 export default getStudentIdsSum;
