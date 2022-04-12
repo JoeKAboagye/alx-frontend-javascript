748339fd67428afbeeb43cb16910553f5ac6da66
Joseph Aboagye <josephkwabenaaboagye@gmail.com>
2022-04-12 16:27:11 +0000

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,19 @@
 export default function getListStudents() {
-    [
-        {
-            id: 1,
-            firstName: 'Guillaume',
-            location: 'San Francisco',
-        },
-        {
-            id: 2,
-            firstName: 'James',
-            location: 'Columbia',
-        },
-        {
-            id: 5,
-            firstName: 'Serena',
-            location: 'San Francisco',
-        },
-    ];
-
-    return getListStudents();
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
 }
