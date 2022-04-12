713a3159139f48794a69cdfc75578b21539e48ee
Joseph Aboagye <josephkwabenaaboagye@gmail.com>
2022-04-12 16:55:17 +0000

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,9 @@
-const getListStudentIds = function (getList) {
-    let listData = [];
-    if (!(getList instanceof Array)) {
-      return listData;
-    };
-    listData = getList.map((results) => results.id);
-    return listData;
-  };
-  export default getListStudentIds;
+const getListStudentIds = (getList) => {
+  let lisdata = [];
+  if (!(getList instanceof Array)) {
+    return lisdata;
+  }
+  lisdata = getList.map((results) => results.id);
+  return lisdata;
+};
+export default getListStudentIds;
