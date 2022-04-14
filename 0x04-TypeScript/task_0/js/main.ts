53e85664152fbc24817cd341bb4a264ee0e27e13
Joseph Aboagye <josephkwabenaaboagye@gmail.com>
2022-04-14 01:37:34 +0000

    Update main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -24,13 +24,13 @@ let studentsList: Student[] = [Student1, Student2]
 const student_table = document.createElement('table');
 const tablebody = document.createElement('tbody');
 
-studentsList.forEach((objectList)=>{
+studentsList.forEach((objList)=>{
     const tablename = document.createElement('tablename');
     const tablerow = document.createElement('tablerow');
     const tablelocation = document.createElement('tablelocation');
 
-    tablename.textContent = objectList.firstName;
-    tablelocation.textContent = objectList.location;
+    tablename.textContent = objList.firstName;
+    tablelocation.textContent = objList.location;
     tablerow.appendChild(tablename);
     tablerow.appendChild(tablelocation);
     tablebody.appendChild(tablerow);
